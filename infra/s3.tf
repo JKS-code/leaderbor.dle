5f35c487d7a0a6bca3abc640c5480321678b1407
willkc15 <kelton.c.williams@gmail.com>
2022-05-29 22:21:59 -0400

    make s3 public acl

--- infra/s3.tf
+++ infra/s3.tf
@@ -3,9 +3,14 @@ resource "aws_s3_bucket" "web_assets" {
   bucket = "${var.project}-web-assets-bucket"
 }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
+resource "aws_s3_bucket_versioning" "web_assets_versioning" {
   bucket = aws_s3_bucket.web_assets.id
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_acl" "web_assets_acl" {
+  bucket = aws_s3_bucket.web_assets.id
+  acl    = "public-read"
 }
